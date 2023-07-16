171375924e2e02cba7d15af85436681108579bba
Shivamnamdev23 <124493494+Shivamnamdev23@users.noreply.github.com>
2023-07-16 02:52:34 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1912,7 +1912,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
             settings = await get_settings(message.chat.id)
             if not files:
-                await client.send_message(req_channel,f"**⊲ #ɪᴍᴅʙ_ʀᴇQᴜᴇꜱᴛᴇᴅ_ᴄᴏɴᴛᴇɴᴛ ⊳\n\n❄️ ᴄᴏɴᴛᴇɴᴛ ɴᴀᴍᴇ :`{search}`\n☀️ ʀᴇQᴜᴇꜱᴛᴇᴅ ʙʏ**: {message.from_user.first_name}\n🔺 ᴜꜱᴇʀ ɪᴅ :{message.from_user.id}\n\n●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎**",
+                await client.send_message(req_channel,f"**⊲ #ᴘʀᴏ_ɪᴍᴅʙ_ʀᴇQᴜᴇꜱᴛᴇᴅ_ᴄᴏɴᴛᴇɴᴛ ⊳\n\n❄️ ᴄᴏɴᴛᴇɴᴛ ɴᴀᴍᴇ :`{search}`\n☀️ ʀᴇQᴜᴇꜱᴛᴇᴅ ʙʏ**: {message.from_user.first_name}\n🔺 ᴜꜱᴇʀ ɪᴅ :{message.from_user.id}\n\n●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎●︎**",
                                                                                                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("●︎ ᴜᴘʟᴏᴀᴅᴇᴅ ᴅᴏɴᴇ ●︎", callback_data="close_data")]]))
                 await m.delete()
                 if settings["spell_check"]:

51fcd07cf9874aa641f0a02d4cf9d815d2275d9c
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-07-16 20:03:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,11 +27,11 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/50e0debf22333841
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/0ef75275f7873e1f842d1.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5651594253').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5864846606').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001979888381').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '5651594253').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '5864846606').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
@@ -43,9 +43,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hegodal811:Q9zSn8CO83Gm68RF@cluster0.cipxa5z.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "onlybotzcrezyk")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'onlybotzcrezyk')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))

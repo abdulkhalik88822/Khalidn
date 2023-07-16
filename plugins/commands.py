7c3997e4e3e4d81d6827428a5c9d39c645ae773b
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-07-16 20:51:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,13 +40,13 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('〆 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 〆', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('✨ ᴜᴘᴅᴀᴛᴇs', url='t.me/crazybotz'),
-                    InlineKeyboardButton('⚜️ ᴅᴇᴠ​', callback_data='owner_info')
+                    InlineKeyboardButton('ʙᴏᴛs', url='t.me/+y5Ab2wjnCVFiMzk1'),
+                    InlineKeyboardButton('ʙᴏᴛ ᴏᴡɴᴇʀ​', callback_data='owner_info')
                 ],[
-                    InlineKeyboardButton('♻️ ʜᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('🎄 ᴀʙᴏᴜᴛ', callback_data='about')
+                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
                 ],[
-                    InlineKeyboardButton('💲 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 💲', callback_data="shortlink_info")
+                    InlineKeyboardButton('💶 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 💸', callback_data="shortlink_info")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(

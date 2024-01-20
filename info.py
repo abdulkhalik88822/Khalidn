17b2c164e589776cefedf82cd4fcfbe3aaa209c5
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2024-01-20 20:16:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5851749250').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001720210775').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002119009885').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -58,12 +58,12 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/bots_up')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/bots_up')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/bots_up')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/updatechannl/2')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/updatechannl/2')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/updatechannl/2')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001720210775'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002119009885'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'bots_up')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
